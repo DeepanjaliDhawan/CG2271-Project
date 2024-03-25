14392981c538df38eefa325e17f91257e1a8e916
ngxzs <seanng01@outlook.com>
2024-03-25 13:01:23 +0800

    feat(motor.c): add init pwm

--- motor.c
+++ motor.c
@@ -4,8 +4,10 @@
 
 #define SW_POS		6		// PORTD Pin 6 (INT)
 
-#define PTB0_Pin 0 // for PWM TPM1 CH0 (PWM)
-#define PTB1_Pin 1
+#define MOTOR_BACK_LEFT		0	// PTB0 TPM1_CH0
+#define MOTOR_BACK_RIGHT 	1	// PTB1 TPM1_CH1
+#define MOTOR_FRONT_LEFT	2	// PTB2 TPM2_CH0
+#define MOTOR_FRONT_RIGHT 	3	// PTB3 TPM2_CH1
 
 #define CLOCK (48000000 / 128) // 375000 (AUDIO PWM)
 #define note_C (uint16_t)(CLOCK / 262)
@@ -29,42 +31,61 @@ static void delay(volatile uint32_t nof) {
   }
 }
 
-/* Init 50 Hz rising edge PWM for PORTB TPM1 CH0,1 */
+/* Init 50 Hz rising edge PWM for PORTB TPM0,1 CH0,1 */
 void InitPWM(void){
 	// Enable Clock Gating for PORTB
 	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
 	
 	// Configure MODE 3 for PWM TPM (163)
-	PORTB->PCR[PTB0_Pin] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[PTB0_Pin] |= PORT_PCR_MUX(3); 
+	PORTB->PCR[MOTOR_BACK_LEFT] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[MOTOR_BACK_LEFT] |= PORT_PCR_MUX(3); 
 	
-	PORTB->PCR[PTB1_Pin] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[PTB1_Pin] |= PORT_PCR_MUX(3);
+	PORTB->PCR[MOTOR_BACK_RIGHT] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[MOTOR_BACK_RIGHT] |= PORT_PCR_MUX(3);
 	
-	// Enable Clock gating for TPM1 (207)
+	PORTB->PCR[MOTOR_FRONT_LEFT] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[MOTOR_FRONT_LEFT] |= PORT_PCR_MUX(3);
+	
+	PORTB->PCR[MOTOR_FRONT_RIGHT] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[MOTOR_FRONT_RIGHT] |= PORT_PCR_MUX(3);
+	
+	// Enable Clock gating for TPM1,2 (207)
 	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
+	SIM->SCGC6 |= SIM_SCGC6_TPM2_MASK;
 	
 	// Select Clock for TPM module
 	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
 	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1); // internal clk
 	
-	// Set Modulo Value 48_000_000(48MHz) / 128 = 375_000 / 7500 = 50 Hz (Clk / MOD = freq) (554)
-	TPM1->MOD = 7500;
-	//TPM1->MOD = 5000;
-	
-	/* Edge-aligned PWM */
-	// Update SnC register : CMOD = 01(up counting), PS = 111 (Prescalar 128) Chapter 31 Timer (553)
+	// init PWM for TIMER 1
+	// up-counting, prescaler = 128 (553)
 	TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
-	TPM1->SC |= TPM_SC_CMOD(1) | TPM_SC_PS(7);
+	TPM1->SC |= TPM_SC_CMOD(1) | TPM_SC_PS(7); 
 	TPM1->SC &= ~(TPM_SC_CPWMS_MASK);
 	
-	// Enable PWM on TPM1 Channel 0 -> PTB0 Edge Align PWM 1010 (555)
-	TPM1_C0SC &= ~ ((TPM_CnSC_ELSB_MASK) |(TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
-	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	// TPM1_CH0 ie MOTOR_BACK_LEFT
+	TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) |(TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1)); // Edge align PWM, high-true pulses: BABA = 1010 (555)
+	// TPM1_CH1 ie MOTOR_BACK_RIGHT
+	TPM1_C1SC &= ~((TPM_CnSC_ELSB_MASK) |(TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM1_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));	
 	
-	// Enable PWM on TPM1 Channel 1-> PTB0
-	TPM1_C1SC &= ~ ((TPM_CnSC_ELSB_MASK) |(TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
-	TPM1_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));		
+	// init PWM for TIMER 2
+	// up-counting, prescaler = 128 (553)
+	TPM2->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM2->SC |= TPM_SC_CMOD(1) | TPM_SC_PS(7); 
+	TPM2->SC &= ~(TPM_SC_CPWMS_MASK);
+	
+	// TPM2_CH0 ie MOTOR_FRONT_LEFT
+	TPM2_C0SC &= ~((TPM_CnSC_ELSB_MASK) |(TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM2_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	// TPM2_CH1 ie MOTOR_FRONT_RIGHT
+	TPM2_C1SC &= ~((TPM_CnSC_ELSB_MASK) |(TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM2_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	// Set Modulo Value 48_000_000(48MHz) / 128 = 375_000 / 7500 = 50 Hz (Clk / MOD = freq) (554)
+	TPM1->MOD = 7500;
+	TPM2->MOD = 7500;
 }
 
 // to select LED for INT code
@@ -115,11 +136,6 @@ int main(void)
 	SystemCoreClockUpdate();
 	InitPWM();
 	InitSwitch();
-
-	// DUTY CYCLE: change VOLUME + can generate different PWM simulataneously (557)
-	// completely HARDWARE driven
-	// TPM1_C0V = 0xEA6; // 0xEA6 = 3750 (half of 7500) -> 50% duty cycle 
-	TPM1_C1V = 0x753; // half of 3750
 	
 	while(1)
 	{
